ab58dc99279ca4a6ab3b2f6c930070e6c7e41b3c
Joshua <joycejosh7@gmail.com>
2021-05-05 15:14:47 -0400

    added delete power function

--- javascripts/power.js
+++ javascripts/power.js
@@ -4,7 +4,20 @@ function appendPowers(powers, element){
 
     for(let power of powers){
         const powerLi = document.createElement("li")
+        const powerDelete = document.createElement("button")
+        powerDelete.innerText = "Power Lost"
         powerLi.innerText = power.ability 
+        powerDelete.addEventListener('click', (e) => deletePower(power.id, powerLi))
+        powerLi.append(powerDelete)
         ul.append(powerLi)
     }
+}
+
+function deletePower(powerId, powerLi){
+    fetch(`http://localhost:3000/powers/${powerId}`,{
+        method: "DELETE"
+    }).then(r => r.json)
+    .then(m => {
+        powerLi.remove()
+    })
 }
